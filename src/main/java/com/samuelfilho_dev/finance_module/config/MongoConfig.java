17ae8e476efdc32de3b14b45b10489eaa33c1f2e
Samuel Filho <snamuelfilho2011@gmail.com>
2026-08-05 02:42:02 -0300

    chore: change config package

--- src/main/java/com/samuelfilho_dev/finance_module/config/MongoConfig.java
+++ src/main/java/com/samuelfilho_dev/finance_module/config/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.samuelfilho_dev.finance_module.confg;
+package com.samuelfilho_dev.finance_module.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
